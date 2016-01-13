b8f6c96888bd1c254dca03696a1286a5df0a45ee
Ryan Boucher <ryan.boucher@distributedlife.com>
2016-01-14 09:24:37 +1100

    make it more es6y

--- src/get-ip-addresses.js
+++ src/get-ip-addresses.js
@@ -1,13 +1,12 @@
 'use strict';
 
 import os from 'os';
-import {map, filter} from 'lodash/collection';
-import {flatten} from 'lodash/array';
+import {map, filter, flatten} from 'lodash';
 
 let ifaces = os.networkInterfaces();
 
 export default function getIpAddresses () {
-  return flatten(map(Object.keys(ifaces), function (ifname) {
+  return flatten(map(Object.keys(ifaces), ifname => {
     let interfaces = filter(ifaces[ifname], {family: 'IPv4', internal: false});
     return map(interfaces, 'address');
   }));
