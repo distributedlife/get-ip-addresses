0c1a8f907e142775b98a499dd05a2247a66f09ee
Ben Callaghan <benj.callaghan@gmail.com>
2017-11-16 09:48:32 -0700

    Added an optional refresh parameter

--- src/get-ip-addresses.js
+++ src/get-ip-addresses.js
@@ -5,7 +5,11 @@ import {map, filter, flatten} from 'lodash';
 
 let ifaces = os.networkInterfaces();
 
-export default function getIpAddresses () {
+export default function getIpAddresses (refresh = false) {
+  if (refresh) {
+    ifaces = os.networkInterfaces();
+  }
+
   return flatten(map(Object.keys(ifaces), ifname => {
     let interfaces = filter(ifaces[ifname], {family: 'IPv4', internal: false});
     return map(interfaces, 'address');
